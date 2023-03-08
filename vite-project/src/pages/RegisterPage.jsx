33b5e26e69e8ce221553f2727e6e469488a80402
nerdnightboy <nerdnightboy@gmail.com>
2023-03-08 11:29:56 +0530

    Stuck with cors connectivity

--- vite-project/src/pages/RegisterPage.jsx
+++ vite-project/src/pages/RegisterPage.jsx
@@ -1,15 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
+    const[name, setName] = useState('');
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    function registerUser(ev){
+        ev.preventDefault();
+        axios.get('http://localhost:4000/test');
+    }
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-32">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form className="max-w-md mx-auto">
-                    <input type="text" placeholder="Jhon Doe" />
-                    <input type="email" placeholder="your@gmail.com" />
-                    <input type="password" placeholder="password" />
-                    <button className="primary">Login</button>
+                <form className="max-w-md mx-auto" onSubmit={registerUser}>
+                    <input type="text"
+                        placeholder="Jhon Doe" 
+                        value={name} 
+                        onChange={ev => setName(ev.target.value)} />
+                    <input type="email" 
+                        placeholder="your@gmail.com" 
+                        value={email}
+                        onChange={ev => setEmail(ev.target.value)} />
+                    <input type="password" 
+                        placeholder="password" 
+                        value={password}
+                        onChange={ev => setPassword(ev.target.value)} />
+                    <button className="primary">Register</button>
                     <div className="text-center py-2 text-gray-500">Already a member? 
                         <Link className="underline text-black p-2" to={'/login'}>Login</Link>
                     </div>
